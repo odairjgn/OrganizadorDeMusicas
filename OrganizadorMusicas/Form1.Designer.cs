0b1722788d94837b5a4958bc1dcbe114f68e0ddb
odairjgn <odairjgn@outlook.com>
2025-05-28 15:00:46 -0300

    Upload

---
+++ OrganizadorMusicas/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace OrganizadorMusicas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirPastaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirPastarecursivamenteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnManter = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.abrirPastaToolStripMenuItem,
+            this.abrirPastarecursivamenteToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "&Arquivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.abrirToolStripMenuItem.Text = "&Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // abrirPastaToolStripMenuItem
+            // 
+            this.abrirPastaToolStripMenuItem.Name = "abrirPastaToolStripMenuItem";
+            this.abrirPastaToolStripMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.abrirPastaToolStripMenuItem.Text = "Abrir &pasta";
+            this.abrirPastaToolStripMenuItem.Click += new System.EventHandler(this.abrirPastaToolStripMenuItem_Click);
+            // 
+            // abrirPastarecursivamenteToolStripMenuItem
+            // 
+            this.abrirPastarecursivamenteToolStripMenuItem.Name = "abrirPastarecursivamenteToolStripMenuItem";
+            this.abrirPastarecursivamenteToolStripMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.abrirPastarecursivamenteToolStripMenuItem.Text = "Abrir pasta &recursivamente";
+            this.abrirPastarecursivamenteToolStripMenuItem.Click += new System.EventHandler(this.abrirPastarecursivamenteToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 325);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 125);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 301);
+            this.panel2.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnExcluir, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnManter, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 125);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnManter
+            // 
+            this.btnManter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnManter.Location = new System.Drawing.Point(3, 3);
+            this.btnManter.Name = "btnManter";
+            this.btnManter.Size = new System.Drawing.Size(94, 119);
+            this.btnManter.TabIndex = 0;
+            this.btnManter.Text = "&Manter (Esc)";
+            this.btnManter.UseVisualStyleBackColor = true;
+            this.btnManter.Click += new System.EventHandler(this.btnManter_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.checkBox1);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.trackBar2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.trackBar1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(103, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(594, 119);
+            this.panel3.TabIndex = 1;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnExcluir.Location = new System.Drawing.Point(703, 3);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(94, 119);
+            this.btnExcluir.TabIndex = 2;
+            this.btnExcluir.Text = "E&xcluir (Delete)";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(800, 301);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.trackBar1.Location = new System.Drawing.Point(0, 0);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(594, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "&Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pa&use";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(131, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(58, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "&Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(429, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 37);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "00:00:00 / 00:00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(196, 73);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(119, 45);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.TickFrequency = 5;
+            this.trackBar2.Value = 100;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(321, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(58, 40);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Mu&te";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Arquivos de áudio|*.mp3;*.wma;*.wav;*.mp4;*.m4a";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(4, 49);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(162, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Sempre questionar ao excluir";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "Organizador de músicas";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirPastaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirPastarecursivamenteToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnManter;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ OrganizadorMusicas/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace OrganizadorMusicas
+{
+    public partial class Form1 : Form
+    {
+        private string[] _extensoes = new[] { "*.mp3", "*.wma", "*.wav", "*.mp4", "*.m4a" };
+        private WMPLib.IWMPPlayer4 _player;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _player = new WMPLib.WindowsMediaPlayer();
+        }
+
+        private void Load(IEnumerable<FileInfo> arquivos)
+        {
+            foreach (var arquivo in arquivos)
+            {
+                var item = new ListViewItem(arquivo.Name);
+                item.Tag = arquivo;
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var arquivos = new List<FileInfo>();
+                foreach (var arquivo in openFileDialog1.FileNames)
+                {
+                    arquivos.Add(new FileInfo(arquivo));
+                }
+                Load(arquivos);
+            }
+        }
+
+        private void abrirPastaToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var pasta = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+                var arquivos = _extensoes
+                    .SelectMany(ext => pasta.GetFiles(ext, SearchOption.TopDirectoryOnly))
+                    .ToArray();
+                Load(arquivos);
+            }
+        }
+
+        private void abrirPastarecursivamenteToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var pasta = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+                var arquivos = _extensoes
+                    .SelectMany(ext => pasta.GetFiles(ext, SearchOption.AllDirectories))
+                    .ToArray();
+                Load(arquivos);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                var arquivo = listView1.SelectedItems[0].Tag as FileInfo;
+                _player.controls.stop();
+                _player.URL = arquivo.FullName;
+                _player.controls.play();
+            }
+        }
+
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+            var tempo = TimeSpan.FromSeconds(_player.controls.currentPosition);
+            var total = TimeSpan.FromSeconds(_player.currentMedia?.duration ?? 0);
+
+            trackBar1.Maximum = (int)total.TotalSeconds > 0 ? (int)total.TotalSeconds : 1;
+            trackBar1.Value = (int)tempo.TotalSeconds > 0 ? (int)tempo.TotalSeconds : 1;
+
+            if (total.TotalSeconds > 0)
+            {
+                label1.Text = $"{tempo:mm\\:ss} / {total:mm\\:ss}";
+            }
+            else
+            {
+                label1.Text = "00:00 / 00:00";
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            var position = trackBar1.Value;
+            _player.controls.currentPosition = position;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            _player.settings.mute = !_player.settings.mute;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            _player.settings.volume = trackBar2.Value;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _player.controls.stop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _player.controls.pause();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _player.controls.play();
+        }
+
+        private void btnManter_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count == 0)
+            {
+                return;
+            }
+
+            var indice = listView1.SelectedIndices[0];
+            var itenToRemove = listView1.SelectedItems[0];
+            listView1.Items.Remove(itenToRemove);
+            listView1.SelectedIndices.Add(indice < listView1.Items.Count ? indice : 0);
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count == 0)
+            {
+                return;
+            }
+
+            if (checkBox1.Checked)
+            {
+                var resposta = MessageBox.Show("Deseja realmente excluir o item selecionado?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (resposta != DialogResult.Yes)
+                {
+                    return;
+                }
+            }
+
+            var indice = listView1.SelectedIndices[0];
+            var itenToRemove = listView1.SelectedItems[0];
+            var arquivo = itenToRemove.Tag as FileInfo;
+            if (arquivo != null && arquivo.Exists)
+            {
+                try
+                {
+                    arquivo.Delete();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao excluir o arquivo: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            listView1.Items.Remove(itenToRemove);
+            listView1.SelectedIndices.Add(indice < listView1.Items.Count ? indice : 0);
+        }
+    }
+}
